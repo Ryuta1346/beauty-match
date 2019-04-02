2e569bd9016525cd8c5746b6634216d51d7492b0
Ryu007 <ryuta.tashiro@gmail.com>
2019-04-02 22:05:13 +0900

    Modify stylist spec

--- spec/factories/stylists.rb
+++ spec/factories/stylists.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     association :salon
     association :category
     name { "HurryPotter" }
+    initialize_with { Stylist.find_or_create_by(name: name)}
     tel { "00000000000" }
     email { "MyString" }
     stylist_since { "2019-04-01" }
