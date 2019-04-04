7cb9a26ab9cf7bf5f5b725353cc5d38bf24f2000
Ryu007 <ryuta.tashiro@gmail.com>
2019-04-04 22:05:39 +0900

    Modify seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #
 
-User.create!(name: "Test User", tel:"08000000000", email: "test_user@example.com", birth_year: "1993")
+User.create!(name: "Test User", tel: "08000000000", email: "test_user@example.com", birth_year: "1993")
 Category.create!(name: "Hair_salon")
 
 Prefecture.create(name: "北海道")
@@ -59,5 +59,23 @@ Prefecture.create(name: "鹿児島")
 Prefecture.create(name: "沖縄")
 Prefecture.create(name: "None")
 
-Salon.create(category_id:1, prefecture_id:48, name:"free", place: "None", tel: "00000000000", manage: "None", num_of_stylists:9999, num_of_sheets:9999, cut_price:0, features:"None")
-Stylist.create!(salon_id:1, category_id:1, name:"Example Stylist", tel:"00000000000", email:"stylisy@example.com", stylist_since: "2000", activity_scope:"渋谷区", cut_price:"6980", features:"カットの再現性")
+Salon.create(category_id:     1,
+             prefecture_id:   48,
+             name:            "free",
+             place:           "None",
+             tel:             "00000000000",
+             manage:          "None",
+             num_of_stylists: 9999,
+             num_of_sheets:   9999,
+             cut_price:       0,
+             features:        "None")
+
+Stylist.create!(salon_id:       1,
+                category_id:    1,
+                name:           "Example Stylist",
+                tel:            "00000000000",
+                email:          "stylisy@example.com",
+                stylist_since:  "2000",
+                activity_scope: "渋谷区",
+                cut_price:      "6980",
+                features:       "カットの再現性")
