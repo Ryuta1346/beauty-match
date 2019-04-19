f3cd5b455f64b807ffddd9a3856b4bcab48ba453
Ryu007 <ryuta.tashiro@gmail.com>
2019-04-20 01:21:13 +0900

    Modify reservations_controller.rb

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,5 @@
 class ReservationsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
   def index
     @salons              = Salon.where.not(id: FREE_SALON).all
